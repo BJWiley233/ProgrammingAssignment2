0b0716a65f7c32ed66f6b9d1d6d59cada4f536a4
BJWiley233 <BJWiley23@hotmail.com>
2017-08-08 22:09:50 -0700

    BJW programmingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## need to install and load these packages first
+install.packages("MASS")
+install.packages("matrixcalc")
+library(MASS) 
+library(matrixcalc) 
 
-## Write a short comment describing this function
+##non-square matrix
+NCols <- sample(3:6, 1)
+NRows <- sample(2:8, 1)
 
-makeCacheMatrix <- function(x = matrix()) {
+myMat <- matrix(runif(NCols*NRows), ncol=NCols)
+is.square.matrix(myMat)
 
+## functions
+makeCacheMatrix <- function(x) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInv <- if (is.square.matrix(x) == TRUE) {
+                function(solve) m <<- solve
+        }
+        else {
+                function(ginv) m <<- ginv
+        }
+        getInv <- function() m
+        list(x, set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <-  if (is.square.matrix(data) == TRUE) {
+                solve(data, ...)
+        }
+        else {
+                ginv(data, ...)
+        }
+        x$setInv(m)
+        m
 }
+
+## run functions for myMat
+notSquare <- makeCacheMatrix(myMat) 
+cacheSolve(notSquare)
+
+
+##check
+ginv(myMat)
